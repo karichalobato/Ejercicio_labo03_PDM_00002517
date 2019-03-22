ec37764daa70bdcfdfdd91c2c29b8a53397b291e
marielosagui <00078913@uca.edu.sv>
2019-03-22 08:52:33 -0600

    Inicio de jercicio labo 03 PDM

---
+++ app/src/main/java/com/example/ejercicio_labo03_pdm_00002517/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.ejercicio_labo03_pdm_00002517;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
